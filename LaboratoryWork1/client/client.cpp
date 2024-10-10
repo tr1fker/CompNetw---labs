f2961320c3e17c2e7608c1e1139e1800a5c04d83
Artyom Letko <cfjhxhkxck@gmail.com>
2024-10-10 18:47:35 +0300

    LabWork1Done

---
+++ LaboratoryWork1/client/client.cpp
@@ -0,0 +1,41 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <windows.h>
+#include <tchar.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    wVersionRequested = MAKEWORD(2, 2);
+    WSAStartup(wVersionRequested, &wsaData);
+    cout << "wVersionRequested = " << wVersionRequested << endl;
+    cout << "Fields of struct wsaData: " << wsaData.wVersion << ' ' << wsaData.wHighVersion
+        << ' ' << wsaData.iMaxSockets << ' ' << wsaData.iMaxUdpDg << endl << endl;
+    struct sockaddr_in peer;
+    peer.sin_family = AF_INET;
+    peer.sin_port = htons(1280);
+    InetPton(AF_INET, _T("127.0.0.1"), &peer.sin_addr.s_addr);
+    SOCKET s = socket(PF_INET, SOCK_STREAM, 0);
+    char buf[255], b[255];
+    connect(s, (struct sockaddr*)&peer, sizeof(peer));
+    while (true){
+        cout << "Введите числа: " << endl;
+        cin.getline(buf, 255, '\n');
+        if (!strlen(buf)) break;
+        send(s, buf, sizeof(buf), 0);
+        if (recv(s, b, sizeof(b), 0) != 0){
+            cout << endl << "Кол-во чисел кратных 3:  " << b << endl << endl;
+        }
+        cin.clear();
+    }
+    closesocket(s);
+    WSACleanup();
+    return 0;
+}
---
+++ LaboratoryWork1/server/server.cpp
@@ -0,0 +1,57 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <windows.h>
+#include <tchar.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    wVersionRequested = MAKEWORD(2, 2);
+    WSAStartup(wVersionRequested, &wsaData);
+    SOCKET socket1 = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(1280);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    int connection = bind(socket1, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    int status_listening = listen(socket1, 5);
+    cout << "Server is running" << endl;
+    char res[255], pocket[255];
+    struct sockaddr_in remote_addr;
+    int size = sizeof(remote_addr);
+    char addr[50];
+    while (true) {
+        SOCKET newSocketConnection = accept(socket1, (struct sockaddr*)&remote_addr, &size);
+        inet_ntop(AF_INET, &(remote_addr.sin_addr), addr, 49);
+        cout << "--------------------------------------------------------" << endl;
+        cout << "client_port = " << ntohs(remote_addr.sin_port) << endl;
+        cout << "client_IP-addr = " << addr << endl << endl;
+        while (recv(newSocketConnection, pocket, sizeof(pocket), 0) != 0){
+            cout << "Введите числа:  " << pocket << endl;
+            int lengthLine = strlen(pocket) + 1, counter = 0;
+            for (int letter = 0, number = 0; letter < lengthLine; letter++) {
+                if (pocket[letter] >= '0' && pocket[letter] <= '9')
+                    number = number * 10 + pocket[letter] - '0';
+                else if (number != 0 && number % 3 == 0)
+                    counter++;
+                else number = 0;
+            }
+            _itoa_s(counter, res, 10);
+            send(newSocketConnection, res, sizeof(res), 0);
+            cout << "Server return:  " << res << endl << endl;
+        }
+        closesocket(newSocketConnection);
+    }
+    WSACleanup();
+    return 0;
+}
+
+
+
