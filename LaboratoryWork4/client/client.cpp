ce56753e303ddccd6f562f3cb6d0bfc4dd4cd491
Artyom Letko <cfjhxhkxck@gmail.com>
2024-10-11 21:41:42 +0300

    Nabrosok

--- LaboratoryWork4/client/client.cpp
+++ LaboratoryWork4/client/client.cpp
@@ -1,1 +1,102 @@
-﻿//client
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+	struct sockaddr_in peer;
+	int s, t, t1;
+	int rc;
+	char buf[256], p, p1, b[256];;
+
+	peer.sin_family = AF_INET;
+	peer.sin_port = htons(7500);
+	peer.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	rc = connect(s, (struct sockaddr*)&peer, sizeof(peer));
+	while (true) {
+		//Выбор пункта меню и отправка его серверу			
+		puts("Choose:");
+		puts("\t1 - Select");
+		puts("\t2 - Edit");
+		puts("\t3 - View");
+		puts("\t4 - Exit");
+		scanf("%s", buf);
+		buf[1] = '\0';
+		send(s, buf, sizeof(buf), 0);
+		p = buf[0];
+
+		switch (p) {
+		case '1': //Выбрать
+			puts("kol-vo months (1-12) :"); scanf("%s", buf);
+			send(s, buf, sizeof(buf), 0);
+			puts("Symbol: "); scanf("%s", buf);
+			send(s, buf, sizeof(buf), 0);
+
+			printf("Sum of taxes: ");
+			recv(s, buf, sizeof(buf), 0);
+			for (t = 0; buf[t + 3]; t++) printf("%c", buf[t]);
+			printf(".");
+			for (t1 = t; buf[t1]; t1++) printf("%c", buf[t1]);
+			printf("\n");
+			break;
+		case '2': //Подредактировать
+
+			puts("What number (1-5) to edit");
+			scanf("%s", buf); //Какой номер будем редактировать			send(s,buf,sizeof(buf),0);
+
+			puts("What field (1-4) to edit");
+			puts("\t1 - Name");
+			puts("\t2 - Number");
+			puts("\t3 - Income");
+			puts("\t4 - Tax");
+			scanf("%s", buf);
+			send(s, buf, sizeof(buf), 0);
+			p1 = buf[0];
+			buf[0] = '\0';
+			switch (p1) {		//Введите новые поля
+			case '1': printf("Name: ");
+				fflush(stdin); fflush(stdout);
+				scanf("%s", b); strcat(buf, b); strcat(buf, " ");
+				scanf("%s", b); strcat(buf, b); strcat(buf, " ");				  scanf("%s", b); strcat(buf, b); strcat(buf, "\0");
+				//puts(buf);
+				send(s, buf, sizeof(buf), 0);
+				break;
+			case '2':fflush(stdin); fflush(stdout);
+				printf("Tab Number: "); scanf("%s", buf);
+				send(s, buf, sizeof(buf), 0);
+				break;
+			case '3':fflush(stdin); fflush(stdout);
+				printf("Income per month: "); scanf("%s", buf);
+				send(s, buf, sizeof(buf), 0);
+				break;
+			case '4':fflush(stdin); fflush(stdout);
+				printf("Tax rate (%) per month: ");
+				scanf("%s", buf);
+				send(s, buf, sizeof(buf), 0);
+			}
+			break;
+		case '3'://Просмотреть 5 записей
+			recv(s, buf, sizeof(buf), 0); printf("%s", buf);
+			recv(s, buf, sizeof(buf), 0); printf("%s", buf);
+			recv(s, buf, sizeof(buf), 0); printf("%s", buf);
+			recv(s, buf, sizeof(buf), 0); printf("%s", buf);
+			recv(s, buf, sizeof(buf), 0); printf("%s", buf);
+			break;
+		case '4'://Выход
+			exit(0);
+		}
+	}
+}
--- LaboratoryWork4/server/server.cpp
+++ LaboratoryWork4/server/server.cpp
@@ -1,1 +1,154 @@
-﻿//server
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+using namespace std;
+#include <sys/types.h>
+//#include<sys/socket.h>
+//#include<sys/signal.h>
+//#include<sys/wait.h>
+//#include<sys/resource.h>
+//#include<netinet/in.h>
+
+#include <string.h>
+#include <stdio.h>
+//#include <unistd.h>
+#include <stdlib.h>
+pid_t fork(void);
+
+struct Employee {
+	char name[35];
+	char number[10];
+	char income[10];
+	char tax[4];
+} em[5];
+
+// процедура для обслуживания соединения
+int Func(int newS) {
+	long int i, num, t, mon, doh, nal;
+	float sum;
+	int m;
+	char p, p1, s;
+	char buf[256], b[256];
+	while (true) {
+		recv(newS, buf, sizeof(buf), 0);
+		p = buf[0];
+		switch (p) {
+		case '1':
+			buf[0] = '\0';
+			sum = 0;
+			recv(newS, buf, sizeof(buf), 0);
+			mon = atoi(buf);
+			recv(newS, buf, sizeof(buf), 0);
+
+			s = buf[0];
+			for (i = 1; i <= 5; i++)
+				if (em[i].name[0] == s) {
+					nal = atoi(em[i].tax);
+					doh = atoi(em[i].income);
+					printf("mon %d\n", mon);
+					sum = sum + (nal * doh * mon) / 100.0;
+				}
+			int* decpt, * sgn;
+			printf("%f\n", sum);
+			strcpy(buf, fcvt(sum, 3, decpt, sgn));
+			send(newS, buf, sizeof(buf), 0);
+			puts(buf);
+			break;
+		case '2':
+			recv(newS, buf, sizeof(buf), 0);  //Номер
+			num = atoi(buf);
+			printf("%d\n", num);
+
+			recv(newS, buf, sizeof(buf), 0);
+			p1 = buf[0];
+			switch (p1) {
+			case '1':
+				recv(newS, buf, sizeof(buf), 0); //Имя
+				strcpy(em[num].name, buf);
+				break;
+			case	'2': recv(newS, buf, sizeof(buf), 0); //Таб.номер
+				strcpy(em[num].number, buf);
+				break;
+			case '3':recv(newS, buf, sizeof(buf), 0); //Доход
+				strcpy(em[num].income, buf);
+				break;
+			case '4': recv(newS, buf, sizeof(buf), 0); //Ставка налога
+				strcpy(em[num].tax, buf);
+			}
+			break;
+		case '3':
+			for (i = 1; i <= 5; i++) {
+				buf[0] = '\0';
+				strcat(buf, em[i].name); strcat(buf, " ");
+				strcat(buf, em[i].number); strcat(buf, " ");
+				strcat(buf, em[i].income); strcat(buf, " ");
+				strcat(buf, em[i].tax); strcat(buf, "\n");
+				send(newS, buf, sizeof(buf), 0);
+			}
+			break;
+		case '4':
+			exit(0);
+
+		}
+	}
+}
+
+
+void reaper(int sig) {
+	int status;
+	while (wait3(&status, WNOHANG, (struct rusage*)0) >= 0);
+}
+
+int main() {
+	strcpy(em[1].name, "Sergeev Sergei Sergeevich");
+	strcpy(em[1].number, "1");
+	strcpy(em[1].income, "100000");
+	strcpy(em[1].tax, "10");
+
+	strcpy(em[2].name, "Ivanov Ivan Ivanovich");
+	strcpy(em[2].number, "2");
+	strcpy(em[2].income, "200000");
+	strcpy(em[2].tax, "20");
+
+	strcpy(em[3].name, "Vladimirov Vladimir Vladmirvich");
+	strcpy(em[3].number, "3");
+	strcpy(em[3].income, "300000");
+	strcpy(em[3].tax, "30");
+
+	strcpy(em[4].name, "Sidorov Sidor Sidorovich ");
+	strcpy(em[4].number, "4");
+	strcpy(em[4].income, "400000");
+	strcpy(em[4].tax, "40");
+
+	strcpy(em[5].name, "Vasilev Vasilii Vasilievich");
+	strcpy(em[5].number, "5");
+	strcpy(em[5].income, "500000");
+	strcpy(em[5].tax, "50");
+
+	struct sockaddr_in local;
+	int s, newS, rc;
+
+	local.sin_family = AF_INET;
+	local.sin_port = htons(7500);
+	local.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	rc = bind(s, (struct sockaddr*)&local, sizeof(local));
+	rc = listen(s, 5);
+	(void)signal(SIGCHLD, reaper);
+	while (true) {
+		newS = accept(s, NULL, NULL);
+		switch (fork()) {
+		case 0:
+			(void)close(s);
+			exit(Func(newS));
+		default:
+			(void)close(newS);
+		}
+	}
+
+	return 0;
+}
